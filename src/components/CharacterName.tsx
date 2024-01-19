b182a7a4cfa22f120b3f58462dc681b84cb7e8a5
pumkko <c.coque@hotmail.fr>
2024-01-18 20:10:26 -0500

    try solid

---
+++ src/components/CharacterName.tsx
@@ -0,0 +1,18 @@
+import { createResource } from "solid-js";
+
+
+export default function CharacterName() {
+    const [name] = createResource(() =>
+        fetch('https://swapi.dev/api/people/1')
+            .then((result) => result.json())
+            .then((data) => data.name)
+    );
+
+    return (
+        <>
+            <h2>Name:</h2>
+            {/* Luke Skywalker */}
+            <div>{name()}</div>
+        </>
+    );
+}
