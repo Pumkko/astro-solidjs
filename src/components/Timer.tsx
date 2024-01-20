f6ae8fa546b0f4b2ad4d65adb3d56f4bb9f145ba
pumkko <c.coque@hotmail.fr>
2024-01-20 07:06:35 -0500

    add nav

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,6 +1,6 @@
 import { createSignal } from "solid-js";
 
-export default function Counter() {
+export default function Timer() {
     const [count, setCount] = createSignal(0);
 
     setInterval(() => setCount(count() + 1), 1000);
